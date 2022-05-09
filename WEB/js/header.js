34a5a611b9b215fc21b928b44e06bbc35ca7cfcb
qwertrty <101600727+qwertrty@users.noreply.github.com>
2022-05-10 08:22:35 +1200

    Add files via upload

---
+++ WEB/js/header.js
@@ -0,0 +1,22 @@
+document.write('<header> <div class="navbar navbar-expand-lg bg-dark navbar-dark">\n' +
+    '        <div class="container-fluid">\n' +
+    '            <!-- company logo and name -->\n' +
+    '            <h1 class="company">\n' +
+    '                <a href="./index.html" title="SeaFood logo" class="navbar-brand">\n' +
+    '                        <img src="../img/logo.png">\n' +
+    '                        <div class="companyname">SeaFood</div>\n' +
+    '                </a>\n' +
+    '            </h1>\n' +
+    '            <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">\n' +
+    '                \n' +
+    '                <div class="navbar-nav ml-auto" >\n' +
+    '                    <!-- navbar link -->\n' +
+    '                    <a href="./Menu.html"  class="nav-item nav-link active">MANU</a>\n' +
+    '                    <a href="./Booking.html"  class="nav-item nav-link active">BOOKING</a>\n' +
+    '                    <a href="./Aboutus.html"  class="nav-item nav-link active">ABOUT US</a>\n' +
+    '                    <a href="./Contactus.html" class="nav-item nav-link active">CONTACT US</a>\n' +
+    '                    <a href="./index.html"  class="nav-item nav-link active">MY ACCOUNT</a>\n' +
+    '                </div>\n' +
+    '            </div>    \n' +
+    '        </div>\n' +
+    '    </div></header>')
